6b5f44e74637f3d24811a2e0dd3bf4cea4560869
Thomfe <th.feneon@laposte.net>
2017-05-28 21:28:47 -0400

    Change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
