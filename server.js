b0f501ef6a21f74b8d70dc31ca58973fe2e3ea81
Thomfe <th.feneon@laposte.net>
2017-05-28 23:55:02 -0400

    First list

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var database = require('./database');
+var bodyParser = require("body-parser");
 
 var port = process.env.PORT || 80;
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+app.use(bodyParser.urlencoded({ extended: true })); //Accept Url encoded params
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
   console.log('Someone is logged');
@@ -21,24 +25,55 @@ app.get('/test', function(req, res){
 //MARK: Add an item
 app.post('/addItem', function(req, res){
 
-
-
   database.connectToDatabase(function(db){
 
   var collection = db.collection('items');
     collection.insertOne({
       title:req.body.title,
-      description: req.body.description,
       done:false,
-      function(err, result) {
-        if (err){
-          console.log("Error adding item",err);
-          return res.json({success: false});
-        }else{
-          console.log("New item !");
-          return res.json({success: true});
-        }
-      }
     });
+  });
+
+  res.write("new Item added");
+});
+
+//MARK: Add an item
+app.post('/checkitem', function(req, res){
+
+  database.connectToDatabase(function(db){
+
+  var collection = db.collection('items');
+  collection.findOneAndUpdate({ title: req.body.title},{ $set: { done: true}}, function(err, itemFound) {
+
+    if (itemFound){
+          res.send();
+      }
+  });
+});
+});
+
+//MARK: get items
+app.get('/jeanpierre', function(req, res){
+
+  database.connectToDatabase(function(db){
+
+  var collection = db.collection('items');
+  var cursor = collection.find()
+
+  var response = ""
+  cursor.each(function(err, item) {
+      // If the item is null then the cursor is exhausted/empty and closed
+      if(item == null) {
+          db.close(); // you may not want to close the DB if you have more code....
+            res.send(response);
+          return;
+      }
+      if (item.done){
+        response = response + "<li class=\"checked\">" + item.title +  "</li>\n"
+        }else{
+        response = response + "<li>" + item.title +  "</li>\n"
+      }
+// otherwise, do something with the item
+  });
   });
 });
